b56f5eb0aba17f488ccf2a2a9ed9853b5f1d3711
Raz Ben Netanel <52315689+raz-bn@users.noreply.github.com>
2020-06-28 20:09:32 +0300

    hotfix

--- exporters/prometheus-dcgm/k8s/pod-gpu-metrics-exporter/src/device_pod.go
+++ exporters/prometheus-dcgm/k8s/pod-gpu-metrics-exporter/src/device_pod.go
@@ -108,8 +108,8 @@ func addPodInfoToMetrics(dir string, srcFile string, destFile string, deviceToPo
 func addPodInfoToLine(originalLine string, pod devicePodInfo) string {
 	//TODO
 	//find a better way to insert hostname
-	hostname, err :=  os.Hostname()
-	if err != nil {hostname = ''}
+	hostname, _ :=  os.Hostname()
+	if err != nil {hostname = "err"}
 	splitOriginalLine := strings.Split(originalLine, "}")
         newLineWithPodName := fmt.Sprintf("%s,pod_name=\"%s\",pod_namespace=\"%s\",container_name=\"%s\",host_name=\"%s\"}%s", splitOriginalLine[0], pod.name, pod.namespace, pod.container, hostname, splitOriginalLine[1])
         return newLineWithPodName
